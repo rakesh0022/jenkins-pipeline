22a466f298ff7ed5c0c83585ef69879b2e5b6c42
engineer <92107358+rakesh0022@users.noreply.github.com>
2024-02-07 01:52:14 +0530

    Create infratest.tf

---
+++ infratest.tf
@@ -0,0 +1,32 @@
+provider "google" {
+  region      = "us-central1"
+}
+
+resource "google_compute_instance" "my_instance" {
+  zone         = "us-central1-a"
+
+machine_type = "n1-standard-16" # <<<<< Try changing this to n1-standard-32 to compare the costs
+
+  scheduling {
+    preemptible = true
+  }
+
+  guest_accelerator {
+    type = "nvidia-tesla-t4" # <<<<< Try changing this to nvidia-tesla-p4 to compare the costs
+    count = 4
+  }
+
+  labels = {
+    Environment = "production"
+    Service = "web-app"
+  }
+}
+
+resource "google_cloudfunctions_function" "my_function" {
+  runtime = "nodejs10"
+  available_memory_mb = 512
+
+  labels = {
+    Environment = "Prod"
+  }
+}
